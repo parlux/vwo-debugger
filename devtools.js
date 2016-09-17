21bd0d6440cacd6a108798465ac890fc8764f817
Paul Carnibella <paulcarnibella@gmail.com>
2016-09-17 22:59:00 +0200

    GH Pages

--- devtools.js
+++
@@ -1,6 +0,0 @@
-// Can use
-// chrome.devtools.*
-// chrome.extension.*
-
-// Create a tab in the devtools area
-chrome.devtools.panels.create("VWO", "vwo-logo.png", "panels/panel.html", function(panel) {});
