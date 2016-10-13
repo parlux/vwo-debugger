f164c1d87454ef80d850c8e32ff6025a6f0da462
Paul Carnibella <paulcarnibella@gmail.com>
2016-10-13 23:29:47 +1100

    Sylin

--- src/app.js
+++ src/app.js
@@ -21,23 +21,23 @@ inspectedTabConnection.on(BrowserActions.NETWORK_REQUEST, (request) => {
   if (request.request.url.includes(vwoConversionUrl)) renderApp()
 })
 
-const $clearCookies = document.querySelector('#cookie-clearer')
-$clearCookies.addEventListener('click', () => {
-  Logger.info('Clearing cookies for ya')
-  Utils.executeCodeInInspectedWindow(`
-    console.log('Clearing VWO cookies')
-    document.cookie.split(';')
-      .filter(f => {
-        if (f.match(/_vwo/) || f.match(/_vis_opt/)) return true
-      })
-      .forEach(cookie => {
-        const domain = '.kitchenwarehouse.com.au'
-        const expires = 'Thu, 01 Jan 1970 00:00:01 GMT'
-        document.cookie = cookie.trim() + ';path=/;domain=' + domain + ';expires=' + expires
-      })
-    window.location.reload()
-  `)
-})
+// const $clearCookies = document.querySelector('#cookie-clearer')
+// $clearCookies.addEventListener('click', () => {
+//   Logger.info('Clearing cookies for ya')
+//   Utils.executeCodeInInspectedWindow(`
+//     console.log('Clearing VWO cookies')
+//     document.cookie.split(';')
+//       .filter(f => {
+//         if (f.match(/_vwo/) || f.match(/_vis_opt/)) return true
+//       })
+//       .forEach(cookie => {
+//         const domain = '.kitchenwarehouse.com.au'
+//         const expires = 'Thu, 01 Jan 1970 00:00:01 GMT'
+//         document.cookie = cookie.trim() + ';path=/;domain=' + domain + ';expires=' + expires
+//       })
+//     window.location.reload()
+//   `)
+// })
 
 // This needs to move to experiments
 function clear() {
